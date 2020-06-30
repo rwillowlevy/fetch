a8cc4332fec61b732c03e7a781d49eae26f436b9
Garrett <garrettgassensmith@gmail.com>
2020-06-30 10:42:19 -0500

    Added gitignore file, small refactor to server.js, changed name in package json files

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
+// ====== DEPENDENCIES ======
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
-const app = express();
+const path = require("path");
+const chalk = require("chalk");
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-// Define middleware here
+// ====== MIDDLEWARE ======
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// ====== ROUTES ======
+app.use(routes)
 
-// Start the API server
+// ====== DATABASE ======
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fetch");
+
+// ====== START SERVER ======
 app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log(chalk.green(`API Server now listening on PORT http://localhost:${PORT}`));
 });
