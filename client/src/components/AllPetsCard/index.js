8e688117adf9a6065208cec2752630bb56e14f3a
Alex B <abailon949@gmail.com>
2020-07-15 19:26:02 -0500

    created all pets componet

---
+++ client/src/components/AllPetsCard/index.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react'
+import { Redirect, useHistory } from 'react-router-dom'
+import store from '../../utils/store'
+import { addPets } from '../../utils/actions'
+import API from '../../utils/API'
+import { Card, CardTitle, Icon, Col, Row, Button } from 'react-materialize'
+import 'materialize-css'
+
+function AllPetCard () {
+  const [ swiped, setSwiped ] = useState(false)
+  const { currentUser, allPets } = store.getState()
+  const possiblePets = allPets.filter(
+    pet => pet._id !== currentUser.pets[0]._id
+  )
+  const currentPet =
+    possiblePets[Math.floor(Math.random() * possiblePets.length)]
+  console.log(currentPet)
+  const newPetState = possiblePets.filter(pet => pet._id !== currentPet._id)
+  store.dispatch(addPets(newPetState))
+  const currentUserPetID = currentUser.pets[0]._id
+  console.log(possiblePets)
+  const likedSwipe = e => {
+    e.preventDefault()
+    const data = {
+      petId: currentUserPetID,
+      targetPetId: currentPet._id,
+      liked: true
+    }
+    const res = API.createSwipe(data)
+    console.log(res)
+    setSwiped(true)
+  }
+  const dislikedSwipe = e => {
+    e.preventDefault()
+    const data = {
+      petId: currentUserPetID,
+      targetPetId: currentPet._id,
+      liked: false
+    }
+    const res = API.createSwipe(data)
+    console.log(res)
+    setSwiped(true)
+  }
+  const renderCard = () => {
+    if (possiblePets.length > 0) {
+      return (
+        <>
+          <Row>
+            <Col m={6} s={12}>
+              <Card
+                closeIcon={<Icon>close</Icon>}
+                header={
+                  <CardTitle image={currentPet.image} reveal waves='light' />
+                }
+                reveal={<p> {currentPet.bio} </p>}
+                revealIcon={<Icon>more_vert</Icon>}
+                title={currentPet.name}
+              >
+                <p>
+                  {currentPet.age} {currentPet.gender} {currentPet.size}{' '}
+                  {currentPet.breed}
+                </p>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col m={3} s={6}>
+              <Button
+                node='button'
+                style={{
+                  marginRight: '5px'
+                }}
+                waves='light'
+                onClick = { dislikedSwipe }
+              >
+                DisLike
+              </Button>
+            </Col>
+            <Col>
+              <Button
+                node='button'
+                style={{
+                  marginRight: '5px'
+                }}
+                waves='light'
+                onClick= { likedSwipe }
+              >
+                Like
+              </Button>
+            </Col>
+          </Row>
+        </>
+      )
+    } else {
+      return <h1> All out of pets! </h1>
+    }
+  }
+  return renderCard()
+}
+
+export default AllPetCard
