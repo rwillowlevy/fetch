e82eb3af7fcba485ccdd5f24bff1fa8c87f6538e
Alex B <abailon949@gmail.com>
2020-07-17 08:47:58 -0500

    Refactored home page

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -6,22 +6,13 @@ import CheckPet from '../../components/CheckPet'
 import store from '../../utils/store'
 import { addAuth, addMatches } from '../../utils/actions'
 import 'materialize-css'
-import { authenticate } from 'passport'
 
 function Home () {
+  // All State Management
   const { checkAuth, setCheckAuth } = store.getState(false) 
   const { currentUser, Auth, matches } = store.getState()
-  let history = useHistory()
-  API.verifyToken(Auth)
-  .then( res => {
-    console.log('user effect');
-    console.log(res)
-  }).catch( err => {
-    console.log(err)
-    store.dispatch(addAuth(undefined))
-    history.push('/')
-  })
   console.log(matches)
+  // UseEffect hook to get matches
   useEffect( () => {
     API.findMatches(currentUser._id)
     .then(res => {
@@ -29,6 +20,16 @@ function Home () {
       store.dispatch(addMatches(res.data))
     })
   }, []);
+  let history = useHistory()
+  // Check user Auth token, if its not vaild send user to home page
+  API.verifyToken(Auth)
+  .then( res => {
+    console.log(res)
+  }).catch( err => {
+    console.log(err)
+    store.dispatch(addAuth(undefined))
+    history.push('/')
+  })
 
   return (
     <Container>
