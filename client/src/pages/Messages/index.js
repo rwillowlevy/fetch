5db959c4007be7fe7f9b928ed3322f4c4c073d71
Alex B <abailon949@gmail.com>
2020-07-16 19:36:48 -0500

    added api call to get all matches

--- client/src/pages/Messages/index.js
+++ client/src/pages/Messages/index.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react'
+import API from '../../utils/API'
+import store from '../../utils/store'
 import { Modal, Col, Container, Row } from 'react-materialize'
 import Modals from '../../components/Modal/index'
 import 'materialize-css'
 
 function Messages () {
+  const { currentUser } = store.getState()
+  useEffect( async() => {
+    const matches = await API.findMatches(currentUser._id)
+    console.log('Match API:', matches)
+  }, []);
   return (
     <div>
 
