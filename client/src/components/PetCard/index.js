7f04f5eac4b5464dfea336508b932f5e5c80d6cf
Alex B <abailon949@gmail.com>
2020-07-19 15:06:14 -0500

    added pet for to profile so user may updated pet info

--- client/src/components/PetCard/index.js
+++ client/src/components/PetCard/index.js
@@ -1,38 +1,185 @@
 import React, { useState } from 'react'
 import { Redirect, useHistory } from 'react-router-dom'
-import { Card, CardTitle, Icon, Col, Row } from 'react-materialize'
+import { TextInput, Select, Checkbox, Textarea, Button } from 'react-materialize'
+import API from '../../utils/API'
+import store from '../../utils/store'
+import { updateCurrentUserPet } from '../../utils/actions'
 import 'materialize-css'
 
-
-function PetCard ({pets}) {
-    const renderCard = () => {
-        if ( pets.length > 0 ) {
-            return (
-                <Row>
-                    <Col
-                        s={12}
-                        className='center'
-                    >
-                        <Card
-                        closeIcon={<Icon>close</Icon>}
-                        header={<CardTitle image={ pets[0].image } reveal waves="light"/>}
-                        reveal={<p> { pets[0].bio } </p>}
-                        revealIcon={<Icon>more_vert</Icon>}
-                        title={ pets[0].name}
-                        >
-                        <p>I am a { pets[0].age } year old { pets[0].gender } { pets[0].breed } about the size of { pets[0].size }</p>
-                        </Card>
-                    </Col>
-                </Row>
-            )
-        }
-        else {
-            return (
-                <h1> You have no pets :(<br/> Add a pet below! </h1>
-            )
-        }
+function PetCard () {
+  const { currentUser, Auth } = store.getState()
+  const [pet, setPet] = useState({
+    name: currentUser.pets[0].name,
+    image: currentUser.pets[0].image,
+    age: currentUser.pets[0].age,
+    size: currentUser.pets[0].size,
+    gender: currentUser.pets[0].gender,
+    bio: currentUser.pets[0].bio,
+    breed: currentUser.pets[0].breed,
+    isVaccinated: currentUser.pets[0].isVaccinated,
+    userId: currentUser._id
+  })
+  const updatePet = async (e) => {
+    e.preventDefault()
+    try{
+        const updatePetRes = await API.updatePet(currentUser.pets[0]._id, pet)
+        console.log(updatePetRes)
+        store.dispatch(updateCurrentUserPet(updatePetRes.data))
     }
-    return renderCard()
+    catch (err) {
+        console.log(err)
+    }
+  }
+  const deletePet = async (e) => {
+    e.preventDefault()
+    try {
+        const removePetRes = await API.removePet(currentUser.pets[0]._id)
+        console.log(removePetRes)
+    }
+    catch (err) {
+        console.log(err)
+    }
+  }
+  const renderCard = () => {
+    if (currentUser.pets.length > 0) {
+      return (
+        <>
+          <img src={pet.image} />
+          <TextInput
+            className='upload-btn'
+            id='TextInput-4'
+            label='Pet Name'
+            value={pet.name}
+            onChange={e => setPet({ ...pet, name: e.target.value })}
+          />
+          <TextInput
+            id='TextInput-4'
+            label='Breed'
+            onChange={e => setPet({ ...pet, breed: e.target.value })}
+            value={pet.breed}
+          />
+          <Select
+            id='Select-9'
+            multiple={false}
+            onChange={e => setPet({ ...pet, age: e.target.value })}
+            options={{
+              classes: '',
+              dropdownOptions: {
+                alignment: 'left',
+                autoTrigger: true,
+                closeOnClick: true,
+                constrainWidth: true,
+                coverTrigger: true,
+                hover: false,
+                inDuration: 150,
+                outDuration: 250
+              }
+            }}
+            value={pet.age}
+          >
+            <option disabled value=''>
+              Age
+            </option>
+            <option value='Puppy'>Puppy</option>
+            <option value='Junior'>Junior</option>
+            <option value='Adult'>Adult</option>
+            <option value='Mature'>Mature</option>
+            <option value='Senior'>Senior</option>
+          </Select>
+          <Select
+            id='Select-9'
+            multiple={false}
+            onChange={e => setPet({ ...pet, size: e.target.value })}
+            options={{
+              classes: '',
+              dropdownOptions: {
+                alignment: 'left',
+                autoTrigger: true,
+                closeOnClick: true,
+                constrainWidth: true,
+                coverTrigger: true,
+                hover: false,
+                inDuration: 150,
+                outDuration: 250
+              }
+            }}
+            value={pet.size}
+          >
+            <option disabled value=''>
+              Pet Size Choose your option
+            </option>
+            <option value='Toy'>Toy (under 12 lbs)</option>
+            <option value='Small'>Small (12-25 lbs)</option>
+            <option value='Medium'>Medium (25-50 lbs)</option>
+            <option value='Large'>Large (50-100 lbs)</option>
+            <option value='Extra Large'>Extra Large(100+ lbs)</option>
+          </Select>
+          <Select
+            id='Select-9'
+            multiple={false}
+            onChange={e => setPet({ ...pet, gender: e.target.value })}
+            options={{
+              classes: '',
+              dropdownOptions: {
+                alignment: 'left',
+                autoTrigger: true,
+                closeOnClick: true,
+                constrainWidth: true,
+                coverTrigger: true,
+                hover: false,
+                inDuration: 150,
+                outDuration: 250
+              }
+            }}
+            value={pet.gender}
+          >
+            <option disabled value=''>
+              Gender
+            </option>
+            <option value='Female'>Female</option>
+            <option value='Male'>Male</option>
+          </Select>
+          <Checkbox
+            id='Checkbox_3'
+            label='Check if your pet is Vaccinated'
+            value='yes'
+            indeterminate
+            onChange={e => setPet({ ...pet, isVaccinated: e.target.checked })}
+          />
+          <Textarea
+            data-length={500}
+            id='Textarea-12'
+            label='Bio'
+            onChange={e => setPet({ ...pet, bio: e.target.value })}
+            value={pet.bio}
+          />
+          <Button
+            node='button'
+            style={{
+              marginRight: '5px'
+            }}
+            waves='light'
+            onClick = { updatePet }
+          >
+            Update Pet
+          </Button>
+          <Button
+            node='button'
+            style={{
+              marginRight: '5px'
+            }}
+            waves='light'
+            onClick= { deletePet }
+          >
+            Delete Pet
+          </Button>
+        </>
+      )
+    } else {
+      return <></>
+    }
+  }
+  return renderCard()
 }
 
 export default PetCard
